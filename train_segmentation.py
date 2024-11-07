5ff563bb93b545b843a427c48e08d767e7c5a40f
EkanshJuneja27 <ekansh.20004@gmail.com>
2024-11-07 15:49:16 +0530

    first commit

--- train_segmentation.py
+++ train_segmentation.py
@@ -64,6 +64,8 @@ class LitUnsupervisedSegmenter(pl.LightningModule):
     def training_step(self, batch, batch_idx):
         # training_step defined the train loop.
         # It is independent of forward
+        current_step = self.global_step
+        print(f"Current Step: {current_step}")
 
         with torch.no_grad():
             img = batch["img"]
